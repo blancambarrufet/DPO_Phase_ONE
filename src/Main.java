5c493656c772bf1b96e3be412c871893feafe696
CarlosMCS97 <carlosmiguel.castro@students.salle.url.edu>
2024-12-06 00:37:58 +0100

    "implementing the main and presentation layer"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import business.CombatManager;
+import presentation.Controller;
+import presentation.UI;
+
 public class Main {
     public static void main(String[] args) {
+        CombatManager combatManager = new CombatManager();
+        UI ui = new UI();
 
-
+        Controller controller = new Controller(ui, combatManager);
+        controller.runMain();
     }
 }
---
+++ src/presentation/MainMenu.java
@@ -0,0 +1,9 @@
+package presentation;
+
+public enum MainMenu {
+    LIST_CHARACTERS,
+    MANAGE_TEAMS,
+    LIST_ITEMS,
+    SIMULATE_COMBAT,
+    EXIT
+}
